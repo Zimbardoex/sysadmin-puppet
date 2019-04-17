cdd1cd84904988d0c47c189d44e78cbd968317de
Zimbardoex <Zimbardoex@gmail.com>
2019-04-17 16:37:11 +1245

    Add memory check hostgroup and service.

--- modules/nagios/manifests/config.pp
+++ modules/nagios/manifests/config.pp
@@ -51,6 +51,29 @@ class nagios::config{
 		notify  => Class["nagios::service"],
 	}
 
+	nagios_hostgroup {'remote-memory':
+		target => '/etc/nagios3/conf.d/ppt_hostgroups.cfg',
+		alias => 'Remote Memory',
+		members => 'group10db.foo.org.nz, group10app.foo.org.nz, group10backups.foo.org.nz, group10mgmt.foo.org.nz',
+		notify  => Class["nagios::service"],
+	}
+
+	nagios_service {'remote-memory-service':
+		service_description => 'Service where memory usage is monitored.',
+		hostgroup_name => 'remote-memory',
+		target => '/etc/nagios3/conf.d/ppt_services.cfg',
+		check_command => 'check_mem',
+		max_check_attempts => 3,
+		retry_check_interval => 1,
+		normal_check_interval => 5,
+		check_period => '24x7',
+		notification_interval => 30,
+		notification_period => '24x7',
+		notification_options => 'w,u,c',
+		contact_groups => 'admins',
+		notify  => Class["nagios::service"],
+	}
+
 	nagios_hostgroup {'remote-disks':	
 		target => '/etc/nagios3/conf.d/ppt_hostgroups.cfg',
 		alias => 'Remote Disks',
