3ed0d7be89d0c18c6a25ad80f0c8aa8a2782aa58
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-05-07 10:54:05 +1245

    Add install classes for bacula-director bacula-file bacula-storage

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,4 @@
-node 'group10db.foo.org.nz', 'group10backups.foo.org.nz', 'group10app.foo.org.nz'{
+node 'group10db.foo.org.nz', 'group10app.foo.org.nz'{
 	package {'vim': ensure => present }
 	include sudo
 	include ntp_service
@@ -7,8 +7,24 @@ node 'group10db.foo.org.nz', 'group10backups.foo.org.nz', 'group10app.foo.org.nz
 	include ssh_authorized
 	include puppet_service
 	include check_resolv
+	include bacula-file
 
 	include nrpe
+}
+
+node 'group10backups.foo.org.nz'{
+	package {'vim': ensure => present }
+	include sudo
+	include ntp_service
+	include mariadb
+	include tidy_files
+	include ssh_authorized
+	include puppet_service
+	include check_resolv
+	include bacula-file
+
+	include nrpe
+	include bacula-storage
 }
 
 node 'group10mgmt.foo.org.nz'{
@@ -20,7 +36,9 @@ node 'group10mgmt.foo.org.nz'{
 	include ssh_authorized
 	include puppet_service
 	include check_resolv
+	include bacula-file
 
 	include nagios
 	include nagios-slack-notifications
+	include bacula-director
 }
