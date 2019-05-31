4490f70944586a80cc0699b41079f552bb80504f
Zimbardoex <Zimbardoex@gmail.com>
2019-05-31 21:30:05 +1245

    Explictly set PHP version for ownCloud dependancies

--- modules/owncloud/manifests/install.pp
+++ modules/owncloud/manifests/install.pp
@@ -1,19 +1,19 @@
 class owncloud::install{
   package {'apache2': ensure => present}
   package {'libapache2-mod-php': ensure => present}
-  package {'php-mysql': ensure => present}
-  package {'php-mbstring': ensure => present}
-  package {'php-gettext': ensure => present}
-  package {'php-intl': ensure => present}
-  package {'php-redis': ensure => present}
-  package {'php-imagick': ensure => present}
-  package {'php-igbinary': ensure => present}
-  package {'php-gmp': ensure => present}
-  package {'php-curl': ensure => present}
-  package {'php-gd': ensure => present}
-  package {'php-zip': ensure => present}
-  package {'php-imap': ensure => present}
-  package {'php-ldap': ensure => present}
-  package {'php-bz2': ensure => present}
-  package {'php-phpseclib': ensure => present}
+  package {'php7.2-mysql': ensure => present}
+  package {'php7.2-mbstring': ensure => present}
+  package {'php7.2-gettext': ensure => present}
+  package {'php7.2-intl': ensure => present}
+  package {'php7.2-redis': ensure => present}
+  package {'php7.2-imagick': ensure => present}
+  package {'php7.2-igbinary': ensure => present}
+  package {'php7.2-gmp': ensure => present}
+  package {'php7.2-curl': ensure => present}
+  package {'php7.2-gd': ensure => present}
+  package {'php7.2-zip': ensure => present}
+  package {'php7.2-imap': ensure => present}
+  package {'php7.2-ldap': ensure => present}
+  package {'php7.2-bz2': ensure => present}
+  package {'php7.2-phpseclib': ensure => present}
 }
