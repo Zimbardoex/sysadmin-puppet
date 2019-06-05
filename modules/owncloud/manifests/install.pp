593dce7e48b77bb3732adf2b8692ba6af6eb3033
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-06-05 22:21:10 +1245

    Change php module versions to 7.0 as that is apache2's default version

--- modules/owncloud/manifests/install.pp
+++ modules/owncloud/manifests/install.pp
@@ -1,19 +1,20 @@
 class owncloud::install{
+# Note: These are all using php7.0 as that is what apache2 defaults to
   package {'apache2': ensure => present}
   package {'libapache2-mod-php': ensure => present}
-  package {'php7.2-mysql': ensure => present}
-  package {'php7.2-mbstring': ensure => present}
-  package {'php7.2-gettext': ensure => present}
-  package {'php7.2-intl': ensure => present}
-  package {'php7.2-redis': ensure => present}
-  package {'php7.2-imagick': ensure => present}
-  package {'php7.2-igbinary': ensure => present}
-  package {'php7.2-gmp': ensure => present}
-  package {'php7.2-curl': ensure => present}
-  package {'php7.2-gd': ensure => present}
-  package {'php7.2-zip': ensure => present}
-  package {'php7.2-imap': ensure => present}
-  package {'php7.2-ldap': ensure => present}
-  package {'php7.2-bz2': ensure => present}
-  package {'php7.2-phpseclib': ensure => present}
+  package {'php7.0-mysql': ensure => present}
+  package {'php7.0-mbstring': ensure => present}
+  package {'php7.0-gettext': ensure => present}
+  package {'php7.0-intl': ensure => present}
+  package {'php7.0-redis': ensure => present}
+  package {'php7.0-imagick': ensure => present}
+  package {'php7.0-igbinary': ensure => present}
+  package {'php7.0-gmp': ensure => present}
+  package {'php7.0-curl': ensure => present}
+  package {'php7.0-gd': ensure => present}
+  package {'php7.0-zip': ensure => present}
+  package {'php7.0-imap': ensure => present}
+  package {'php7.0-ldap': ensure => present}
+  package {'php7.0-bz2': ensure => present}
+  package {'php7.0-xml': ensure => present}
 }
